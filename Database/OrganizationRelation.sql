f5ab2ab059fead71c7c545c0fb6f8dc4f178344e
Lukas Lansky <lukas@lansky.name>
2015-03-28 12:44:14 +0100

    OrganizationRelation

---
+++ Database/OrganizationRelation.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[OrganizationRelation]
+(
+	[Id] INT NOT NULL PRIMARY KEY,
+	[SupOrganization] INT NOT NULL,
+	[SubOrganization] INT NOT NULL,
+	CONSTRAINT [FK_OrganizationRelation_Organization_Sup] FOREIGN KEY ([SupOrganization]) REFERENCES [Organization]([Id]),
+	CONSTRAINT [FK_OrganizationRelation_Organization_Sub] FOREIGN KEY ([SubOrganization]) REFERENCES [Organization]([Id]),
+	CONSTRAINT [AK_SupOrganization_SubOrganization] UNIQUE ([SupOrganization], [SubOrganization])
+)
