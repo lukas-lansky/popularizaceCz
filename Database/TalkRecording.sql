6d0c53587d006712c6aea835b65b6c88215cf027
Lukáš Lánský <lukas@lansky.name>
2015-09-18 23:47:30 +0200

    Showing YouTube recordings at left side of the talk detail page

--- Database/TalkRecording.sql
+++ Database/TalkRecording.sql
@@ -1,7 +1,9 @@
 ﻿CREATE TABLE [dbo].[TalkRecording]
 (
-	[TalkId] INT NOT NULL PRIMARY KEY IDENTITY(1, 1),
+	[TalkRecordingId] INT NOT NULL PRIMARY KEY IDENTITY(1, 1),
+	[TalkId] INT NOT NULL,
 	[Url] NVARCHAR(1024) NULL,
 	[YouTubeVideoId] NVARCHAR(32) NULL,
+	[Description] NVARCHAR(1024) NULL,
 	CONSTRAINT [FK_TalkRecording_Talk] FOREIGN KEY ([TalkId]) REFERENCES [Talk]([Id])
 )
--- src/PopularizaceCz.DataLayer/Entities/TalkRecordingDbEntity.cs
+++ src/PopularizaceCz.DataLayer/Entities/TalkRecordingDbEntity.cs
@@ -5,11 +5,15 @@ namespace PopularizaceCz.DataLayer.Entities
     public class TalkRecordingDbEntity : IDbEntity
     {
         public int Id {
-            get { return this.TalkId; }
-            set { this.TalkId = value; }
+            get { return this.TalkRecordingId; }
+            set { this.TalkRecordingId = value; }
         }
 
+        public int TalkRecordingId { get; set; }
+
         public int TalkId { get; set; }
+
+        public string Description { get; set; }
 
         public string Url { get; set; }
 
