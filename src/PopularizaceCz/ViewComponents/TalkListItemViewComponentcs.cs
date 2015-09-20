9327d43f19b2acc43ab5b1a1754a3663b6ec323f
Lukáš Lánský <lukas@lansky.name>
2015-09-20 19:15:08 +0200

    Nicer talk lists using view components

---
+++ src/PopularizaceCz/ViewComponents/TalkListItemViewComponentcs.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNet.Mvc;
+using PopularizaceCz.DataLayer.Entities;
+
+namespace PopularizaceCz.ViewComponents
+{
+    public sealed class TalkListItemViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(TalkDbEntity talk)
+        {
+            return View(talk);
+        }
+    }
+}
